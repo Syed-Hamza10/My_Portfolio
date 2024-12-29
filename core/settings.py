bcdd039020edaf322f01e7b9c34ac39e852a9bfa
Hamza <124598247+Syed-Hamza10@users.noreply.github.com>
2024-12-30 00:12:53 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-j8w9p0y12w34ghigmpssoqey2c2^t2@r&9umab9j*%_6_jurkl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['HamzaFaiz.pythonanywhere.com']
 
 
 # Application definition
