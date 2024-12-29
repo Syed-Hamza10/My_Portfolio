a12a5f3261d0d1ea17e531d1ca8aa81041cd8fc9
Syed-Hamza10 <syed.hamza.ds@gmail.com>
2024-12-29 23:45:49 +0500

    Added Requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j8w9p0y12w34ghigmpssoqey2c2^t2@r&9umab9j*%_6_jurkl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
